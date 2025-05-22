4d9adc15797886d2210b8069b5005b306493fae0
Lucas Capdevila <lcapdevila60@gmail.com>
2025-05-21 21:21:51 -0300

    prueba

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -33,7 +33,7 @@ const Login = () => {
       <div className="w-full max-w-md space-y-8 rounded-lg bg-white p-6 shadow-lg">
         <div>
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            Ingreso de Administrador
+            Ingresar
           </h2>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
